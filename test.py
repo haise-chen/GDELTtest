de23794b6db3e3fd1d50b4ae3565e3a5b7af695b
Haise <76421606+haise-chen@users.noreply.github.com>
2023-05-07 22:03:35 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,169 +1,103 @@
+import csv
+import pandas as pd
+
+# 定义要读取的CSV文件列表和要读取的列
+file_list = ['./201001.csv', './201002.csv', './201003.csv',
+             './201004.csv', './201005.csv', './201006.csv',
+             './201007.csv', './201008.csv', './201009.csv',
+             './201010.csv', './201011.csv', './201012.csv']
+country_index = 51
+type_index = 29  # 例如，读取第一列的值
+score_index = 30  # 例如，读取第二列的值
+time_index = 31  # 例如，读取第二列的值
+# 初始化字典
+M_dict = {}
+N_dict = {}
+NMT_dict = {}
+MMT_dict = {}
+ABS_dict = {}
+a_dict = {}
+b_dict = {}
+c_dict = {}
+i = 0
+# 循环遍历文件列表并逐个读取CSV文件
+for file_name in file_list:
+
+    fo = open(file_name, 'r', newline='', encoding='utf-8-sig')  # 打开csv文件
+    ls = []
+    for line in fo:
+        line = line.replace("\n", "").replace("\r", "")  # 去掉将换行符
+        # columns = [x for x in line.split("\t") if x]
+        ls.append(line.split("\t"))  # 以\t为分隔符
+    for line in ls:
+        country = line[51]
+        mytype = line[29]
+        score = abs(float(line[30]))
+        times = int(line[31])
+        if country:
+            if not M_dict.get(country):
+                M_dict[country] = 0
+            if not MMT_dict.get(country):
+                MMT_dict[country] = 0
+
+
+            M_dict[country] = M_dict[country] + 1
+
+            MMT_dict[country] = MMT_dict[country] + times
+
+            if mytype in ["3", "4"]:
+                if not N_dict.get(country):
+                    N_dict[country] = 0
+                if not ABS_dict.get(country):
+                    ABS_dict[country] = 0
+                if not NMT_dict.get(country):
+                    NMT_dict[country] = 0
+                # 计算国家冲突新闻总数N
+                N_dict[country] = N_dict[country] + 1
+                # 计算国家冲突新闻分值
+                ABS_dict[country] = ABS_dict[country] + score
+                # 计算国家冲突新闻提及次数
+                NMT_dict[country] = NMT_dict[country] + times
+
+    i += 1
+    print(i)
+
+
+for k, v in N_dict.items():
+    # 该country总num
+    if not a_dict.get(k):
+        a_dict[k] = 0
+    a_dict[k] = float(ABS_dict[k] / v)
+
+for k, v in N_dict.items():
+    # 该country总num
+    if not b_dict.get(k):
+        b_dict[k] = 0
+    b_dict[k] = float(NMT_dict[k] / MMT_dict[k])
+
+for k, v in M_dict.items():
+    # 该country总num
+    if not c_dict.get(k):
+        c_dict[k] = 0
+    if a_dict.get(k) and b_dict.get(k):
+        c_dict[k] = a_dict[k]*b_dict[k]
+''''
+for key, value in c_dict.items():
+    print(key, value)
 '''
-该代码适用于新闻年份文件
-即到2005年为止
-'''
+
+content = []
+for k, v in c_dict.items():
+    content.append([k, str(v)])
+with open("./2010result.csv", 'w', newline='') as f:
+    # 写入文件
+    for line in content:
+        try:
+            # 以逗号分隔一行的每个元素，后接一个换行符
+            f.write(",".join(line) + "\n")
+
+        except Exception as e:
+            print(e)
 
 
-import os
-from functools import reduce
 
-
-class Analyze(object):
-    def __init__(self, csv_path):
-        self.csv_path = csv_path
-        self.csv_content = self.csv2json()
-        self.countrya = {}
-        self.countryb = {}
-        self.countryc = {}
-        self.country = {}
-
-    def csv2json(self):
-        fo = open(self.csv_path, 'r', newline='', encoding='utf-8-sig')  # 打开csv文件
-        ls = []
-        for line in fo:
-            line = line.replace("\n", "").replace("\r", "")  # 去掉将换行符
-            # columns = [x for x in line.split("\t") if x]
-            ls.append(line.split("\t"))  # 以\t为分隔符
-        return ls
-        # print(ls[9])
-        # print(len(ls[9]), ls[9][29:32], ls[9][44])
-
-    def calc_q1(self):
-        country_34_num = {}
-        country_num = {}
-        country_31_num = {}
-        for line in self.csv_content:
-            country = line[51]
-            c30 = line[29]
-            c31 = line[30]
-            if country:
-                if not self.country.get(country):
-                    self.country[country] = 0
-                self.country[country] = self.country[country] + 1
-
-                if c30 in ["3", "4"]:
-                    if not country_34_num.get(country):
-                        country_34_num[country] = 0
-                    country_34_num[country] = country_34_num[country] + 1
-                '''
-                if c31:
-                    if not country_31_num.get(country):
-                        country_31_num[country] = 0
-                    country_31_num[country] = country_34_num[country] + abs(int(c31))
-                '''
-        for k, v in country_34_num.items():
-            '''
-            # 该country总num
-            print("country {} c30 3/4 num: {}, summary num: {}".format(k, v, country_num[k]))
-            '''
-        return country_num, country_34_num
-
-    def calc_q2(self):
-        country_34_num = {}
-        country_num = {}
-        country_31_num = {}
-        for line in self.csv_content:
-            country = line[51]
-            c30 = line[29]
-            c31 = line[30]
-            if country:
-                if not country_num.get(country):
-                    country_num[country] = 0
-                country_num[country] = country_num[country] + 1
-
-                if c30 in ["3", "4"]:
-                    if not country_34_num.get(country):
-                        country_34_num[country] = 0
-                    country_34_num[country] = country_34_num[country] + 1
-                    if c31:
-                        if not country_31_num.get(country):
-                            country_31_num[country] = 0
-                        res = abs(float(c31))
-                        country_31_num[country] = country_31_num[country] + res
-
-        for k, v in country_34_num.items():
-            # 该country总num
-            if not self.countrya.get(k):
-                self.countrya[k] = 0
-            self.countrya[k] = float(country_31_num[k] / v)
-            #print("country {} c30 3/4 num: {}, summary num: {}".format(k, v, self.a))
-        return country_num, country_34_num
-
-    def calc_q3(self):
-        country_34_num = {}
-        country_num = {}
-        country_32_num = {}
-        country_32_34_num = {}
-        for line in self.csv_content:
-            country = line[51]
-            c30 = line[29]
-            c32 = line[31]
-            if country:
-                if not country_num.get(country):
-                    country_num[country] = 0
-                country_num[country] = country_num[country] + 1
-
-                if c32:
-                    if not country_32_num.get(country):
-                        country_32_num[country] = 0
-                    res = int(c32)
-                    country_32_num[country] = country_32_num[country] + res
-
-                if c30 in ["3", "4"]:
-                    if not country_34_num.get(country):
-                        country_34_num[country] = 0
-                    country_34_num[country] = country_34_num[country] + 1
-                    if c32:
-                        if not country_32_34_num.get(country):
-                            country_32_34_num[country] = 0
-                        res = int(c32)
-                        country_32_34_num[country] = country_32_34_num[country] + res
-
-        for k, v in country_32_34_num.items():
-            # 该country总num
-            if not self.countryb.get(k):
-                self.countryb[k]=0
-            self.countryb[k] = float(country_32_34_num[k] / country_32_num[k])
-            #print("country {} c30 3/4 num: {}, summary num: {}".format(k, v, country_num[k]))
-        return self
-
-    def calc_q4(self):
-        for k, v in self.countrya.items():
-            # 该country总num
-            if not self.countryc.get(k):
-                self.countryc[k] = 0
-            if self.countrya.get(k) and self.countryb.get(k):
-                self.countryc[k] = self.countrya[k] * self.countryb[k]
-            print("country {} c num: {},".format(k, self.countryc[k]))
-
-    def calc(self):
-        # res1 = self.calc_q1()
-
-        self.calc_q1()
-        self.calc_q2()
-        self.calc_q3()
-        self.calc_q4()
-        content = []
-        for k, v in self.countryc.items():
-            content.append([k, str(v)])
-        self.json2csv(content=content)
-
-    @staticmethod
-    def json2csv(content):
-        """
-        content参数是两层嵌套的list
-        """
-        with open("./1995result.csv", 'w', newline='') as f:
-            # 写入文件
-            for line in content:
-                try:
-                    # 以逗号分隔一行的每个元素，后接一个换行符
-                    f.write(",".join(line) + "\n")
-
-                except Exception as e:
-                    print(e)
-
-
-if __name__ == '__main__':
-    a = Analyze(csv_path="./1995.csv")
-    a.calc()
